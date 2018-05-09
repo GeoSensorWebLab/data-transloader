e44b292b1b6d7b2680b92937379a6b042cd81d1a
James Badger <james@jamesbadger.ca>
2018-05-08 22:06:04 -0600

    Use GET/PATCH/POST on Datastream entity

--- lib/transloader/datastream.rb
+++ lib/transloader/datastream.rb
@@ -31,9 +31,52 @@ module Transloader
       })
     end
 
+    # Similar to `to_json` but does not include linked entities.
+    def attributes_to_json
+      JSON.generate({
+        description: @description,
+        name: @name,
+        observationType: @observation_type,
+        unitOfMeasurement: @unit_of_measurement
+      })
+    end
+
+    # Check if self is a subset of entity.
+    # Cycling through JSON makes the keys the same order and all stringified.
+    def same_as?(entity)
+      JSON.parse(self.attributes_to_json) == JSON.parse(JSON.generate({
+        description: entity['description'],
+        name: entity['name'],
+        observationType: entity['observationType'],
+        unitOfMeasurement: entity['unitOfMeasurement']
+      }))
+    end
+
     def upload_to(url)
       upload_url = self.join_uris(url, "Datastreams")
-      self.post_to_path(upload_url)
+
+      filter = "name eq '#{@name}' and description eq '#{@description}'"
+      response = self.get(URI(upload_url + "?$filter=#{filter}"))
+      body = JSON.parse(response.body)
+
+      # Look for matching existing entities. If no entities match, use POST to
+      # create a new entity. If one or more entities match, then the first is
+      # re-used. If the matching entity has the same name/description but
+      # different encodingType/metadata, then a PATCH request is used to
+      # synchronize.
+      if body["@iot.count"] == 0
+        self.post_to_path(upload_url)
+      else
+        existing_entity = body["value"].first
+        @link = existing_entity['@iot.selfLink']
+        @id = existing_entity['@iot.id']
+
+        if same_as?(existing_entity)
+          puts "Re-using existing Datastream entity."
+        else
+          self.patch_to_path(URI(@link))
+        end
+      end
     end
   end
 end
