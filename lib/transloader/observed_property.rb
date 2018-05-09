0a2e76a5fd737ce2c4a60456b6046a0ecee52ca6
James Badger <james@jamesbadger.ca>
2018-05-08 22:00:57 -0600

    Use GET/PATCH/POST for ObservedProperty entity

--- lib/transloader/observed_property.rb
+++ lib/transloader/observed_property.rb
@@ -24,9 +24,41 @@ module Transloader
       })
     end
 
+    # Check if self is a subset of entity.
+    # Cycling through JSON makes the keys the same order and all stringified.
+    def same_as?(entity)
+      JSON.parse(self.to_json) == JSON.parse(JSON.generate({
+        name: entity['name'],
+        description: entity['description'],
+        definition: entity['definition']
+      }))
+    end
+
     def upload_to(url)
       upload_url = self.join_uris(url, "ObservedProperties")
-      self.post_to_path(upload_url)
+
+      filter = "name eq '#{@name}' and description eq '#{@description}'"
+      response = self.get(URI(upload_url + "?$filter=#{filter}"))
+      body = JSON.parse(response.body)
+
+      # Look for matching existing entities. If no entities match, use POST to
+      # create a new entity. If one or more entities match, then the first is
+      # re-used. If the matching entity has the same name/description but
+      # different encodingType/metadata, then a PATCH request is used to
+      # synchronize.
+      if body["@iot.count"] == 0
+        self.post_to_path(upload_url)
+      else
+        existing_entity = body["value"].first
+        @link = existing_entity['@iot.selfLink']
+        @id = existing_entity['@iot.id']
+
+        if same_as?(existing_entity)
+          puts "Re-using existing ObservedProperty entity."
+        else
+          self.patch_to_path(URI(@link))
+        end
+      end
     end
   end
 end
