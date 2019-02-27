65fe9b990f4049216f24367f5a6846ffd991f277
James Badger <james@jamesbadger.ca>
2019-02-27 15:50:09 -0700

    Upload entities to SensorThings API
    Also use string keys for the Hash, as when loaded from a file it
    will have string keys so loading from variable should also use
    strings.

--- lib/transloader/data_garrison_station.rb
+++ lib/transloader/data_garrison_station.rb
@@ -18,6 +18,7 @@ module Transloader
       @metadata = {}
       @metadata_path = "#{@provider.cache_path}/#{DataGarrisonProvider::CACHE_DIRECTORY}/metadata/#{@user_id}/#{@id}.json"
       @observations_path = "#{@provider.cache_path}/#{DataGarrisonProvider::CACHE_DIRECTORY}/#{@user_id}/#{@id}"
+      @base_path = "https://datagarrison.com/users/#{@user_id}/#{@id}/index.php"
     end
 
     # Download and extract metadata from HTML, use to build metadata 
@@ -132,17 +133,52 @@ module Transloader
 
       # Convert to Hash
       @metadata = {
-        name: "Data Garrison Station #{@id}",
-        description: "Data Garrison Weather Station #{@id}",
-        latitude: nil,
-        longitude: nil,
-        elevation: nil,
-        updated_at: nil,
-        datastreams: datastream_metadata,
-        transceiver: transceiver_metadata,
-        logger: logger_metadata,
-        download_links: download_links,
-        properties: @properties
+        'name'           => "Data Garrison Station #{@id}",
+        'description'    => "Data Garrison Weather Station #{@id}",
+        'latitude'       => nil,
+        'longitude'      =>  nil,
+        'elevation'      =>  nil,
+        'updated_at'     =>  nil,
+        # example datastream item:
+        # {
+        #   "id": "Pressure",
+        #   "Units": "mbar",
+        #   "Resolution": "12 bit",
+        #   "Part number": "S-BPA-XXXX",
+        #   "Range": "660.000 to 1069.400mbar",
+        #   "Serial number": "3513109",
+        #   "Sub and serial number": "3513109"
+        # }
+        'datastreams'    =>  datastream_metadata,
+        # example transceiver metadata:
+        # {
+        #   "id": "transceiver",
+        #   "ID": "300234065673960",
+        #   "Status": "active",
+        #   "Power level": "100 %",
+        #   "Transmission interval": "every 120 minutes",
+        #   "Mode": "standby unless power falls below 75 %",
+        #   "Low power alarm": "triggers if power falls below 30 %",
+        #   "Sensor alarm(s)": "on",
+        #   "Network": "Satellite",
+        #   "Board revision": "0xA0"
+        # }
+        'transceiver'    =>  transceiver_metadata,
+        # example logger metadata:
+        # {
+        #   "id": "logger",
+        #   "Serial number": "20301990",
+        #   "Logging start": "08/12/18 10:58 am local time",
+        #   "Logging interval": "every 15.00 minutes",
+        #   "Sampling interval": "off",
+        #   "Launch description": "Test Launch",
+        #   "Part number": "S-BPA-XXXX",
+        #   "Data start address": "1105",
+        #   "Logging power": "Yes"
+        # }
+        'logger'         =>  logger_metadata,
+        'download_links' =>  download_links,
+        'properties'     =>  @properties
       }
     end
 
@@ -150,15 +186,145 @@ module Transloader
     # If the station data is already cached, use that. If not, download and
     # save to a cache file.
     def get_metadata
+      if File.exist?(@metadata_path)
+        @metadata = JSON.parse(IO.read(@metadata_path))
+      else
+        @metadata = download_metadata
+        save_metadata
+      end
     end
 
-    # Connect to Environment Canada and download SWOB-ML
+    # Connect to Data Garrison and download Observations
     def get_observations
     end
 
     # Upload metadata to SensorThings API
     def put_metadata(server_url)
-      puts "Uploading to STA WIP"
+      # THING entity
+      # Create Thing entity
+      thing = Thing.new({
+        name:        @metadata['name'],
+        description: @metadata['description'],
+        properties:  {
+          logger:      @metadata['logger'],
+          provider:    "Data Garrison",
+          station_id:  @id,
+          station_url: @base_path,
+          transceiver: @metadata['transceiver'],
+          user_id:     @user_id
+        }
+      })
+
+      # Upload entity and parse response
+      thing.upload_to(server_url)
+
+      # Cache URL
+      @metadata['Thing@iot.navigationLink'] = thing.link
+      save_metadata
+
+      # LOCATION entity
+      # Check if latitude or longitude are blank
+      if @metadata['latitude'].nil? || @metadata['longitude'].nil?
+        puts "ERROR: Station latitude or longitude is nil!"
+        puts "Location entity cannot be created. Exiting."
+        exit(1)
+      end
+      
+      # Create Location entity
+      location = Location.new({
+        name:         @metadata['name'],
+        description:  @metadata['description'],
+        encodingType: 'application/vnd.geo+json',
+        location: {
+          type:        'Point',
+          coordinates: [@metadata['longitude'].to_f, @metadata['latitude'].to_f]
+        }
+      })
+
+      # Upload entity and parse response
+      location.upload_to(thing.link)
+
+      # Cache URL
+      @metadata['Location@iot.navigationLink'] = location.link
+      save_metadata
+
+      # SENSOR entities
+      @metadata['datastreams'].each do |stream|
+        # Create Sensor entities
+        sensor = Sensor.new({
+          name:        "Station #{@id} #{stream['id']} Sensor",
+          description: "Data Garrison Station #{@id} #{stream['id']} Sensor",
+          # This encoding type is a lie, because there are only two types in
+          # the spec and none apply here. Implementations are strict about those
+          # two types, so we have to pretend.
+          # More discussion on specification that could change this:
+          # https://github.com/opengeospatial/sensorthings/issues/39
+          encodingType: 'application/pdf',
+          metadata:     @base_path
+        })
+
+        # Upload entity and parse response
+        sensor.upload_to(server_url)
+
+        # Cache URL and ID
+        stream['Sensor@iot.navigationLink'] = sensor.link
+        stream['Sensor@iot.id'] = sensor.id
+      end
+
+      save_metadata
+
+      # OBSERVED PROPERTY entities
+      @metadata['datastreams'].each do |stream|
+        # Create Observed Property entities
+        # TODO: Use mapping to improve these entities
+        observed_property = ObservedProperty.new({
+          name:        stream['id'],
+          definition:  "http://example.org/#{stream['id']}",
+          description: stream['id']
+        })
+
+        # Upload entity and parse response
+        observed_property.upload_to(server_url)
+
+        # Cache URL
+        stream['ObservedProperty@iot.navigationLink'] = observed_property.link
+        stream['ObservedProperty@iot.id'] = observed_property.id
+      end
+
+      save_metadata
+
+      # DATASTREAM entities
+      @metadata['datastreams'].each do |stream|
+        # Create Datastream entities
+        # TODO: Use mapping to improve these entities
+        datastream = Datastream.new({
+          name:        "Station #{@id} #{stream['id']}",
+          description: "Data Garrison Station #{@id} #{stream['id']}",
+          # TODO: Use mapping to improve unit of measurement
+          unitOfMeasurement: {
+            name:       stream['Units'] || "",
+            symbol:     stream['Units'] || "",
+            definition: ''
+          },
+          # TODO: Use more specific observation types, if possible
+          observationType: 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Observation',
+          Sensor: {
+            '@iot.id' => stream['Sensor@iot.id']
+          },
+          ObservedProperty: {
+            '@iot.id' => stream['ObservedProperty@iot.id']
+          }
+        })
+
+        # Upload entity and parse response
+        datastream.upload_to(thing.link)
+
+        # Cache URL
+        stream['Datastream@iot.navigationLink'] = datastream.link
+        stream['Datastream@iot.id'] = datastream.id
+      end
+
+      save_metadata
     end
 
     # Upload station observations for `date` to the SensorThings API server at
