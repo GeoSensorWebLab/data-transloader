0a6c03650b04a46c3a8b91a20aa28ec58cf424ea
James Badger <james@jamesbadger.ca>
2019-06-19 15:56:32 -0600

    Add support for PUT requests
    Some STA implementations don't fully support PATCH, so they might
    work with PUT.

--- lib/sensorthings/entity.rb
+++ lib/sensorthings/entity.rb
@@ -59,32 +59,7 @@ module SensorThings
 
     def patch_to_path(url)
       request = Net::HTTP::Patch.new(url)
-      request.body = self.to_json
-      request.content_type = 'application/json'
-
-      # Log output of request
-      puts "#{request.method} #{request.uri}"
-      puts "Content-Type: #{request.content_type}"
-      puts self.to_json
-      puts ''
-
-      response = Net::HTTP.start(url.hostname, url.port) do |http|
-        http.open_timeout = 1800
-        http.read_timeout = 1800
-        http.request(request)
-      end
-
-      # Force encoding on response body
-      # See https://bugs.ruby-lang.org/issues/2567
-      response.body = response.body.force_encoding('UTF-8')
-
-      # Log output of response
-      puts "HTTP/#{response.http_version} #{response.message} #{response.code}"
-      response.each do |header, value|
-        puts "#{header}: #{value}"
-      end
-      puts response.body
-      puts ''
+      response = send_request(request)
 
       if response.class != Net::HTTPOK && response.class != Net::HTTPNoContent
         raise "Error: Could not PATCH entity. #{url}\n #{response.body}\n #{request.body}"
@@ -94,6 +69,33 @@ module SensorThings
 
     def post_to_path(url)
       request = Net::HTTP::Post.new(url)
+      response = send_request(request)
+
+      if response.class != Net::HTTPCreated
+        raise "Error: Could not POST entity. #{url}\n #{response.body}\n #{request.body}"
+        exit 2
+      end
+
+      @link = response['Location']
+      @id = JSON.parse(response.body)['@iot.id']
+    end
+
+    def put_to_path(url)
+      request = Net::HTTP::Put.new(url)
+      response = send_request(request)
+
+      if response.class != Net::HTTPCreated
+        raise "Error: Could not PUT entity. #{url}\n #{response.body}\n #{request.body}"
+        exit 2
+      end
+
+      @link = response['Location']
+      @id = JSON.parse(response.body)['@iot.id']
+    end
+
+    # PATCH/POST/PUT all use the same behaviour for sending data, so it
+    # can be re-used in this method.
+    def send_request(request)
       request.body = self.to_json
       request.content_type = 'application/json'
 
@@ -121,13 +123,7 @@ module SensorThings
       puts response.body
       puts ''
 
-      if response.class != Net::HTTPCreated
-        raise "Error: Could not POST entity. #{url}\n #{response.body}\n #{request.body}"
-        exit 2
-      end
-
-      @link = response['Location']
-      @id = JSON.parse(response.body)['@iot.id']
+      response
     end
   end
 end
