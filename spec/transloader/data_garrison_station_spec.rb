aef166a7669e9221f5b32ade03774325da822cab
James Badger <james@jamesbadger.ca>
2019-07-02 12:27:24 -0600

    Add specs for metadata get/put

--- spec/transloader/data_garrison_station_spec.rb
+++ spec/transloader/data_garrison_station_spec.rb
@@ -71,4 +71,99 @@ RSpec.describe Transloader::DataGarrisonStation do
       end
     end
   end
+
+  ##############
+  # Put Metadata
+  ##############
+  
+  context "Uploading Metadata" do
+    # pre-create the station for this context block
+    before(:each) do
+      reset_cache($cache_dir)
+      @provider = nil
+      @station = nil
+
+      VCR.use_cassette("data_garrison/station") do
+        @provider = Transloader::DataGarrisonProvider.new($cache_dir)
+        @station = @provider.get_station(
+          user_id: "300234063581640",
+          station_id: "300234065673960"
+        )
+        # These values must be fixed before uploading to STA.
+        @station.metadata[:latitude] = 69.158
+        @station.metadata[:longitude] = -107.0403
+        @station.metadata[:timezone_offset] = "-06:00"
+        # Fix for error in source data
+        @station.metadata[:datastreams].last[:id] = "Battery Voltage"
+        @station.save_metadata
+      end
+
+      @sensorthings_url = "http://192.168.33.77:8080/FROST-Server/v1.0/"
+    end
+
+    it "creates a Thing entity and caches the entity URL" do
+      VCR.use_cassette("data_garrison/metadata_upload") do
+        @station.upload_metadata(@sensorthings_url)
+
+        expect(WebMock).to have_requested(:post, 
+          "#{@sensorthings_url}Things").once
+        expect(@station.metadata[:"Thing@iot.navigationLink"]).to_not be_empty
+      end
+    end
+
+    it "creates a Location entity and caches the entity URL" do
+      VCR.use_cassette("data_garrison/metadata_upload") do
+        @station.upload_metadata(@sensorthings_url)
+
+        expect(WebMock).to have_requested(:post, 
+          %r[#{@sensorthings_url}Things\(\d+\)/Locations]).once
+        expect(@station.metadata[:"Location@iot.navigationLink"]).to_not be_empty
+      end
+    end
+
+    it "creates Sensor entities and caches the URLs" do
+      VCR.use_cassette("data_garrison/metadata_upload") do
+        @station.upload_metadata(@sensorthings_url)
+
+        expect(WebMock).to have_requested(:post, 
+          %r[#{@sensorthings_url}Sensors]).at_least_once
+        expect(@station.metadata[:datastreams][0][:"Sensor@iot.navigationLink"]).to_not be_empty
+      end
+    end
+
+    it "creates Observed Property entities and caches the URLs" do
+      VCR.use_cassette("data_garrison/metadata_upload") do
+        @station.upload_metadata(@sensorthings_url)
+
+        expect(WebMock).to have_requested(:post, 
+          %r[#{@sensorthings_url}ObservedProperties]).at_least_once
+        expect(@station.metadata[:datastreams][0][:"ObservedProperty@iot.navigationLink"]).to_not be_empty
+      end
+    end
+
+    it "maps the source observed properties to standard observed properties" do
+      pending
+      fail
+    end
+
+    it "creates Datastream entities and caches the URLs" do
+      VCR.use_cassette("data_garrison/metadata_upload") do
+        @station.upload_metadata(@sensorthings_url)
+
+        expect(WebMock).to have_requested(:post, 
+          %r[#{@sensorthings_url}Things\(\d+\)/Datastreams]).at_least_once
+        expect(@station.metadata[:datastreams][0][:"Datastream@iot.navigationLink"]).to_not be_empty
+      end
+    end
+
+    it "maps the source observation type to O&M observation types on Datastreams" do
+      pending
+      fail
+    end
+
+    it "maps the source observation type to standard UOMs on Datastreams" do
+      pending
+      fail
+    end
+  end
 end
