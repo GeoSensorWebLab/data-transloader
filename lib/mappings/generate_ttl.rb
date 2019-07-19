eeb1d758020e695f7572cd95e52febb260b45f16
James Badger <james@jamesbadger.ca>
2019-07-19 16:54:48 -0600

    Fix Provider matching in conversion script

--- lib/mappings/generate_ttl.rb
+++ lib/mappings/generate_ttl.rb
@@ -12,51 +12,75 @@ path = ARGV[0]
 mapping = JSON.parse(IO.read(path))
 output = ""
 
+# Escape any double-quotes in the input string.
 def e(str)
   str.gsub('"', '\"')
 end
 
+# Convert spaces to underscores in the input string.
 def u(str)
   str.gsub(" ", "_")
 end
 
 # Generate ObservedProperty
 op = mapping["ObservedProperty"]
-output += <<-EOH
-###  http://gswlab.ca/ontologies/etl-ontology#Property:#{u(op["name"])}
-etl-ontology:Property:#{u(op["name"])} rdf:type owl:NamedIndividual ,
-                                                  etl-ontology:ObservedProperty ;
-                                         etl-ontology:definition "#{op["definition"]}"^^xsd:anyURI ;
-                                         etl-ontology:description "#{e(op["description"])}"^^xsd:string .
 
-EOH
+# Only create entry if entity is defined. Entity may be undefined if
+# an appropriate mapping cannot be found.
+if op["name"] != ""
+  output += <<-EOH
+  ###  http://gswlab.ca/ontologies/etl-ontology#Property:#{u(op["name"])}
+  etl-ontology:Property:#{u(op["name"])} rdf:type owl:NamedIndividual ,
+                                                    etl-ontology:ObservedProperty ;
+                                           etl-ontology:definition "#{op["definition"]}"^^xsd:anyURI ;
+                                           etl-ontology:description "#{e(op["description"])}"^^xsd:string .
+
+  EOH
+end
 
 # Generate UOM
 uom = mapping["UnitOfMeasurement"]
+
+# Only create entry if entity is defined. Entity may be undefined if
+# an appropriate mapping cannot be found.
+if uom["name"] != ""
 output += <<-EOH
-###  http://gswlab.ca/ontologies/etl-ontology#Unit:#{u(uom["name"])}
-etl-ontology:Unit:#{u(uom["name"])} rdf:type owl:NamedIndividual ,
-                                etl-ontology:UnitOfMeasurement ;
-                       etl-ontology:definition "#{uom["definition"]}"^^xsd:anyURI ;
-                       etl-ontology:name "#{uom["name"]}"^^xsd:string ;
-                       etl-ontology:symbol "#{uom["symbol"]}"^^xsd:string .
+  ###  http://gswlab.ca/ontologies/etl-ontology#Unit:#{u(uom["name"])}
+  etl-ontology:Unit:#{u(uom["name"])} rdf:type owl:NamedIndividual ,
+                                  etl-ontology:UnitOfMeasurement ;
+                         etl-ontology:definition "#{uom["definition"]}"^^xsd:anyURI ;
+                         etl-ontology:name "#{uom["name"]}"^^xsd:string ;
+                         etl-ontology:symbol "#{uom["symbol"]}"^^xsd:string .
 
-EOH
-
-# Generate SourceProperty
-provider = case mapping["provider"]
-when "data_garrison" then "DataGarrison"
-when "environment_canada" then "EnvironmentCanada"
-when "data_garrison" then "DataGarrison"
+  EOH
 end
 
-output += <<-EOH
-###  http://gswlab.ca/ontologies/etl-ontology##{provider}:#{u(mapping["id"])}
-etl-ontology:DataGarrison:#{u(mapping["id"])} rdf:type owl:NamedIndividual ,
-                                              etl-ontology:SourceProperty ;
-                                     etl-ontology:matchesObservedProperty etl-ontology:Property:#{u(op["name"])} ;
-                                     etl-ontology:matchesUnitOfMeasurement etl-ontology:Unit:#{u(uom["name"])} .
+# Generate SourceProperty
+provider = case mapping["DataProvider"]
+when "data_garrison" then "DataGarrison"
+when "environment_canada" then "EnvironmentCanada"
+when "campbell_scientific" then "CampbellScientific"
+else raise
+end
 
-EOH
+# Check if destination entities are missing. If they are, then don't
+# link to nothing.
+if op["name"] != "" && uom["name"] != ""
+  output += <<-EOH
+  ###  http://gswlab.ca/ontologies/etl-ontology##{provider}:#{u(mapping["id"])}
+  etl-ontology:#{provider}:#{u(mapping["id"])} rdf:type owl:NamedIndividual ,
+                                                etl-ontology:SourceProperty ;
+                                       etl-ontology:matchesObservedProperty etl-ontology:Property:#{u(op["name"])} ;
+                                       etl-ontology:matchesUnitOfMeasurement etl-ontology:Unit:#{u(uom["name"])} .
+
+  EOH
+else
+  output += <<-EOH
+  ###  http://gswlab.ca/ontologies/etl-ontology##{provider}:#{u(mapping["id"])}
+  etl-ontology:#{provider}:#{u(mapping["id"])} rdf:type owl:NamedIndividual ,
+                                                etl-ontology:SourceProperty .
+
+  EOH
+end
 
 puts output
