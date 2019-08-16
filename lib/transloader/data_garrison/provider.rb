e748fb2ee409deed3da1371caaf81f5989cf9947
James Badger <james@jamesbadger.ca>
2019-08-16 17:02:55 -0600

    Rename new_station to get_station for DataGarrison

--- lib/transloader/data_garrison/provider.rb
+++ lib/transloader/data_garrison/provider.rb
@@ -11,31 +11,8 @@ module Transloader
       @http_client = http_client
     end
 
-    # Create a new Station object based on the station ID, and
-    # automatically load its metadata from data source or file
+    # Create a new Station object based on the station ID
     def get_station(user_id:, station_id:)
-      store_opts = {
-        cache_path: @cache_path,
-        provider: PROVIDER_NAME,
-        station: "#{user_id}-#{station_id}"
-      }
-      data_store     = DataStore.new(store_opts)
-      metadata_store = MetadataStore.new(store_opts)
-
-      stn = DataGarrisonStation.new(
-        data_store: data_store,
-        http_client: @http_client,
-        id: station_id,
-        metadata_store: metadata_store,
-        properties: { user_id: user_id },
-        provider: self)
-      stn.get_metadata
-      stn
-    end
-
-    # Create a new Station object based on the station ID.
-    # Does not load any metadata.
-    def new_station(user_id:, station_id:)
       store_opts = {
         cache_path: @cache_path,
         provider: PROVIDER_NAME,
--- spec/transloader/data_garrison_provider_spec.rb
+++ spec/transloader/data_garrison_provider_spec.rb
@@ -9,27 +9,15 @@ RSpec.describe Transloader::DataGarrisonProvider do
     @http_client = Transloader::HTTP.new
   end
 
-  it "creates a station object with the given user id and station id" do
+  it "initializes a new station without loading any metadata" do
     VCR.use_cassette("data_garrison/station") do
       provider = Transloader::DataGarrisonProvider.new($cache_dir, @http_client)
       station = provider.get_station(
         user_id: "300234063581640",
         station_id: "300234065673960"
       )
-
       expect(station.id).to eq("300234065673960")
       expect(station.properties[:user_id]).to eq("300234063581640")
-      expect(station.metadata).to_not eq({})
-    end
-  end
-
-  it "initializes a new station without loading any metadata" do
-    VCR.use_cassette("data_garrison/station") do
-      provider = Transloader::DataGarrisonProvider.new($cache_dir, @http_client)
-      station = provider.new_station(
-        user_id: "300234063581640",
-        station_id: "300234065673960"
-      )
       expect(station.metadata).to eq({})
     end
   end
