cbdeec7766e66775c4eb950b15bef24d10d76e48
James Badger <james@jamesbadger.ca>
2019-08-21 11:07:31 -0600

    Add spec for MetadataStore

---
+++ spec/transloader/metadata_store_spec.rb
@@ -0,0 +1,67 @@
+require 'transloader'
+
+require 'fileutils'
+require 'json'
+require 'rspec'
+require 'time'
+
+RSpec.describe Transloader::MetadataStore do
+  before(:each) do
+    reset_cache($cache_dir)
+  end
+
+  it "creates the storage path on initialization" do
+    expect(Dir.exists?("#{$cache_dir}/v2/test/metadata")).to be false
+
+    Transloader::MetadataStore.new(
+      cache_path: $cache_dir,
+      provider: "test",
+      station: "unique")
+
+    expect(Dir.exists?("#{$cache_dir}/v2/test/metadata")).to be true
+  end
+
+  it "returns empty metadata if the cache is empty" do
+    store = Transloader::MetadataStore.new(
+      cache_path: $cache_dir,
+      provider: "test",
+      station: "unique")
+
+    expect(store.metadata).to eq({})
+  end
+
+  it "will store metadata in the file cache" do
+    store = Transloader::MetadataStore.new(
+      cache_path: $cache_dir,
+      provider: "test",
+      station: "unique")
+    store.set("test", "value")
+    raw_data = JSON.parse(IO.read("#{$cache_dir}/v2/test/metadata/unique.json"))
+
+    expect(raw_data["metadata"]["test"]).to eq("value")
+  end
+
+  it "returns metadata from the file cache" do
+    store = Transloader::MetadataStore.new(
+      cache_path: $cache_dir,
+      provider: "test",
+      station: "unique")
+    store.set("test", "value")
+
+    expect(store.get("test")).to eq("value")
+  end
+
+  it "supports deep merging metadata attributes" do
+    store = Transloader::MetadataStore.new(
+      cache_path: $cache_dir,
+      provider: "test",
+      station: "unique")
+    store.set(:test, { test2: { test3: "1", test4: "2" } })
+    store.merge({
+      test: { test2: { test3: "5" } }
+    })
+
+    expect(store.metadata[:test][:test2][:test3]).to eq("5")
+    expect(store.metadata[:test][:test2][:test4]).to eq("2")
+  end
+end
