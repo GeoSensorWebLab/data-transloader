b9c3981d87364977588b8551c70b5f952ad4e3a0
James Badger <james@jamesbadger.ca>
2020-04-30 15:36:31 -0600

    Parse metadata from CSV headers
    As data was collected using Campbell Scientific weather stations,
    we can re-use that parsing functionality. However in this case we
    can auto-populate some of the metadata as we *know* the location
    of all the historical KLRS weather data already.
    
    In the next refactor the parsing functionality for the
    CampbellScientific module and the KLRSHistoricalWeather module
    should be extracted to a Ruby mixin.

--- lib/transloader/klrs_historical_weather/station.rb
+++ lib/transloader/klrs_historical_weather/station.rb
@@ -27,8 +27,105 @@ module Transloader
     # If `override_metadata` is specified, it is merged on top of the 
     # existing metadata before being cached.
     def download_metadata(override_metadata: {}, overwrite: false)
-      # TODO
-      raise "Unimplemented Method"
+      if (@metadata_store.metadata != {} && !overwrite)
+        logger.warn "Existing metadata found, will not overwrite."
+        return false
+      end
+
+      data_paths  = @properties[:data_paths]
+      if data_paths.empty?
+        raise Error, "No paths to data files specified. Data files are required to load station metadata."
+      end
+
+      data_files  = []
+      datastreams = []
+
+      data_paths.each do |path|
+        # Open file for reading. If it does not exist, skip it.
+        if !File.exists?(path)
+          logger.warn "Cannot load data from path: '#{path}'"
+          next
+        end
+
+        # Important: Encoding must be set before opening the file. Here
+        # we convert to UTF-8 as that is what our database will expect.
+        data = CSV.read(path, { encoding: "windows-1252:utf-8" })
+
+        # Store CSV file metadata
+        data_files.push(DataFile.new({
+          url:           File.absolute_path(path),
+          last_modified: to_iso8601(File.mtime(path)),
+          length:        File.size(path)
+        }).to_h)
+
+        # Parse CSV headers for station metadata
+        # 
+        # Row 1:
+        # 1. File Type
+        # 2. Station Name
+        # 3. Model Name
+        # 4. Serial Number
+        # 5. Logger OS Version
+        # 6. Logger Program
+        # 7. Logger Program Signature
+        # 8. Table Name
+        # 
+        # Note: It is possible that different files may have different
+        # station metadata values. We are assuming that all data files
+        # are from the same station/location and that the values are not
+        # different between data files.
+        @properties[:station_model_name]    = data[0][2]
+        @properties[:station_serial_number] = data[0][3]
+        @properties[:station_program]       = data[0][5]
+
+        # Parse CSV column headers for datastreams, units
+        # 
+        # Row 2:
+        # 1. Timestamp
+        # 2+ (Observed Property)
+        # Row 3:
+        # Unit or Data Type
+        # Row 4:
+        # Observation Type (peak value, average value)
+        data[1].slice(1..-1).each_with_index do |col, index|
+          datastreams.push({
+            name: col,
+            units: data[2][1+index],
+            type: data[3][1+index]
+          })
+        end
+      end
+
+      # Reduce datastreams to unique entries, as multiple data files 
+      # *may* share the same properties
+      datastreams.uniq! do |datastream|
+        datastream[:name]
+      end
+
+      # Warn about incomplete metadata
+      logger.warn "Sensor metadata PDF or SensorML not available from data source."
+      logger.warn "The URL may be manually added to the station metadata file under the \"procedure\" key."
+
+      # Convert to Hash
+      @metadata = {
+        name:            "KLRS Weather Station #{@id}",
+        description:     "KLRS Historical Weather Station #{@id}",
+        latitude:        61.02741,
+        longitude:       -138.41071,
+        elevation:       nil,
+        timezone_offset: "-07:00",
+        updated_at:      Time.now.utc,
+        procedure:       nil,
+        datastreams:     datastreams,
+        data_files:      data_files,
+        properties:      @properties
+      }
+
+      if !override_metadata.nil?
+        @metadata.merge!(override_metadata)
+      end
+
+      save_metadata
     end
 
     # Upload metadata to SensorThings API.
