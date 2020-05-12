4d0b70f0ae50597161a18df83340ce0cf2681783
James Badger <james@jamesbadger.ca>
2020-05-12 10:45:50 -0600

    Add local server testing script for energy data
    Allows scripting of end-to-end process when testing with a local
    STA server on a VM.

---
+++ test/klrs_h_energy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+
+# This is a very simple bash script for testing the command line
+# interface with a local SensorThings API server.
+
+SCRIPT=transload
+PROVIDER="klrs_h_energy"
+STATION_ID="KLRS_Office_Energy"
+DATASTORE="datastore/weather"
+DESTINATION="http://192.168.33.77:8080/FROST-Server/v1.0/"
+
+# KLRS Historical Energy Usage Data Test Run
+ruby $SCRIPT get metadata \
+    --provider $PROVIDER \
+    --station_id $STATION_ID \
+    --data_path "tmp/klrs-energy/gen_april.xls" \
+    --data_path "tmp/klrs-energy/gen_may.xls" \
+    --data_path "tmp/klrs-energy/gen_jun.xls" \
+    --data_path "tmp/klrs-energy/gen_july.xls" \
+    --data_path "tmp/klrs-energy/gen_aug.xls" \
+    --data_path "tmp/klrs-energy/gen_sep.xls" \
+    --cache $DATASTORE \
+    --overwrite
+
+ruby $SCRIPT put metadata \
+    --provider $PROVIDER \
+    --station_id $STATION_ID \
+    --cache $DATASTORE \
+    --destination $DESTINATION
+
+ruby $SCRIPT get observations \
+    --provider $PROVIDER \
+    --station_id $STATION_ID \
+    --cache $DATASTORE
+
+# A limited interval is used to prevent this from taking too long during
+# a test.
+ruby $SCRIPT put observations \
+    --provider $PROVIDER \
+    --station_id $STATION_ID \
+    --cache $DATASTORE \
+    --date "2014-04-01T00:00:00Z/2014-05-01T00:00:00Z" \
+    --destination $DESTINATION
