0336fd02bed083f6347952aae7d6abea7c1f17d2
James Badger <james@jamesbadger.ca>
2020-06-04 16:57:23 -0600

    Fix specs for FileMetadataStore
    Was still using the old arguments API.

--- spec/transloader/metadata_stores/file_metadata_store_spec.rb
+++ spec/transloader/metadata_stores/file_metadata_store_spec.rb
@@ -8,33 +8,34 @@ require 'time'
 RSpec.describe Transloader::FileMetadataStore do
   before(:each) do
     reset_cache($cache_dir)
+    @database_url = "file://#{$cache_dir}"
   end
 
   it "creates the storage path on initialization" do
     expect(Dir.exists?("#{$cache_dir}/test/metadata")).to be false
 
     Transloader::FileMetadataStore.new(
-      cache_path: $cache_dir,
-      provider: "test",
-      station: "unique")
+      database_url: @database_url,
+      provider_key: "test",
+      station_key: "unique")
 
     expect(Dir.exists?("#{$cache_dir}/test/metadata")).to be true
   end
 
   it "returns empty metadata if the cache is empty" do
     store = Transloader::FileMetadataStore.new(
-      cache_path: $cache_dir,
-      provider: "test",
-      station: "unique")
+      database_url: @database_url,
+      provider_key: "test",
+      station_key: "unique")
 
     expect(store.metadata).to eq({})
   end
 
   it "will store metadata in the file cache" do
     store = Transloader::FileMetadataStore.new(
-      cache_path: $cache_dir,
-      provider: "test",
-      station: "unique")
+      database_url: @database_url,
+      provider_key: "test",
+      station_key: "unique")
     store.set("test", "value")
     raw_data = JSON.parse(IO.read("#{$cache_dir}/test/metadata/unique.json"))
 
@@ -43,9 +44,9 @@ RSpec.describe Transloader::FileMetadataStore do
 
   it "returns metadata from the file cache" do
     store = Transloader::FileMetadataStore.new(
-      cache_path: $cache_dir,
-      provider: "test",
-      station: "unique")
+      database_url: @database_url,
+      provider_key: "test",
+      station_key: "unique")
     store.set("test", "value")
 
     expect(store.get("test")).to eq("value")
@@ -53,9 +54,9 @@ RSpec.describe Transloader::FileMetadataStore do
 
   it "supports deep merging metadata attributes" do
     store = Transloader::FileMetadataStore.new(
-      cache_path: $cache_dir,
-      provider: "test",
-      station: "unique")
+      database_url: @database_url,
+      provider_key: "test",
+      station_key: "unique")
     store.set(:test, { test2: { test3: "1", test4: "2" } })
     store.merge({
       test: { test2: { test3: "5" } }
