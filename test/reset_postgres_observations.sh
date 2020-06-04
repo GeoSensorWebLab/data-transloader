4894fe27ae3c63553674e978749cc16fd67ec126
James Badger <james@jamesbadger.ca>
2020-06-04 15:59:52 -0600

    Add explanation to comment

--- test/reset_postgres_observations.sh
+++ test/reset_postgres_observations.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Deletes all observations cached in Postgres on the "etl" database.
+# This is used to quickly reset a single table for testing the loading
+# observations step ("get observations").
 set -e
 
 psql etl -c "TRUNCATE observations;"
