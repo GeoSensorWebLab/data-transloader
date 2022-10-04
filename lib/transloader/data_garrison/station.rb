5a3daf1ff4d1246c3dbd6689599f0d0bd08309f9
Christian Autermann <c.autermann@52north.org>
2022-10-04 10:46:12 +0200

    store a link to datagarrison page in sensors

--- lib/transloader/data_garrison/station.rb
+++ lib/transloader/data_garrison/station.rb
@@ -64,7 +64,7 @@ module Transloader
       html.xpath('/html/body/table/tr[position()=2]/td/table/tr/td[position()=2]/div[position()=2]/table/tr[position()=2]/td/table/tr/td/font/a').collect do |element|
         href       = element.attr('href')
         data_desc  = element.text.to_s
-        filename   = data_desc.sub(/ /, '_')
+        filename   = data_desc.gsub(/ /, '_')
 
         if filename == ""
           # Without a filename, the file cannot be downloaded as a TSV
@@ -273,6 +273,8 @@ module Transloader
         # Create Sensor entities
         sensor = build_sensor("Station #{@id} #{stream[:name]} Sensor", "#{NAME} #{@id} #{stream[:name]} Sensor")
 
+        sensor.encoding_type = "text/html"
+        sensor.metadata = "https://datagarrison.com/users/#{@user_id}/#{@id}/"
         # Upload entity and parse response
         sensor.upload_to(server_url)
 
